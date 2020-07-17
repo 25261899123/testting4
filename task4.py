dc0ddad112bd8c81346e530fd69bd93e66a0097b
marlen <kf-ku@mail.ru>
2020-07-17 12:16:05 +0600

    initial

---
+++ task4.py
@@ -0,0 +1,13 @@
+"""Исходный список содержит положительные и отрицательные числа. Требуется положительные
+поместить в один список, а отрицательные - в другой."""
+Spisok = [1,2,3,-1,-2,-3]
+qwe = len(Spisok)
+qwe1 = qwe /2
+qwe2 = int(qwe1)
+print (qwe2)
+ewq = (Spisok[:qwe2])
+ewq2 = (Spisok[qwe2:])
+new_list = (ewq)
+new_list2 = (ewq2)
+print(ewq)
+print(ewq2)
